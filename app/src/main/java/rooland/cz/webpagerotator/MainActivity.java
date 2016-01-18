3098ee2b506aa26b2745c2cd1b1d2a3e5672dda3
Mailo Svetel <development@rooland.cz>
2016-01-18 09:20:43 +0100

    Somehow working, but deformed

--- app/src/main/java/rooland/cz/webpagerotator/MainActivity.java
+++ app/src/main/java/rooland/cz/webpagerotator/MainActivity.java
@@ -3,18 +3,24 @@ package rooland.cz.webpagerotator;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-    private Handler handler;
-    private Timer timer;
-    private WebView webview;
+    public Handler handler;
+    public Timer timer;
+    public WebView webview;
+
+
+
+    public WebView getWebview() {
+        return webview;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         webview_setup();
 
 
-        Integer web_load_interval = 60_000 * 5;
+        Integer web_load_interval = 1_000 * 5;
 
         ProgressBar timer_progress_bar = timer_progress_setup(web_load_interval);
         timer_setup(web_load_interval, timer_progress_bar);
@@ -48,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
         PlannedWebLoad task = new PlannedWebLoad(
                 this,
                 timer_progress_bar,
-                time_one_percent
+                time_one_percent,
+                handler
         );
         task.timer_progress_bar = timer_progress_bar;
         task.fraction = time_one_percent;
@@ -67,6 +74,63 @@ public class MainActivity extends AppCompatActivity {
         webview.setWebChromeClient(new WebChromeClient());
 
         webview.loadUrl("http://www.rooland.cz");
+    }
+
+
+    public class PlannedWebLoad extends TimerTask {
+        public AppCompatActivity localActivity;
+
+        public Integer pages_position;
+        public ProgressBar timer_progress_bar;
+        public Integer fraction;
+        public String[] pages;
+        public Handler handler;
+
+
+
+        public PlannedWebLoad(AppCompatActivity activity, ProgressBar inputProgressBar, Integer inputFraction, Handler inputHandler) {
+            pages = new String[] {
+                    "http://www.rooland.cz/tag/dev.html",
+                    "http://www.rooland.cz/tag/komunikace.html",
+                    "http://www.rooland.cz/tag/zacatecnik.html"
+            };
+
+            pages_position = 0;
+            localActivity = activity;
+            timer_progress_bar = inputProgressBar;
+            fraction = inputFraction;
+            handler = inputHandler;
+        }
+
+        @Override
+        public void run() {
+            handler.post(new Runnable() {
+                public void run() {
+                    // This method will be called from another thread,and Contacts.Intents.UI work must
+                    // happen in the main thread,so we dispatch it via a Handler object.
+
+                    timer_progress_bar.incrementProgressBy(fraction);
+
+                    if (timer_progress_bar.getProgress() == timer_progress_bar.getMax()) {
+                        timer_progress_bar.setProgress(0);
+                        webview.loadUrl(next_page());
+                    }
+                }
+            });
+        }
+
+        public String next_page() {
+
+            String next_page_address = pages[pages_position];
+
+            if(pages_position > pages.length) {
+                pages_position += 1;
+            } else {
+                pages_position = 0;
+            }
+
+            return next_page_address;
+        }
     }
 
 }
--- app/src/main/java/rooland/cz/webpagerotator/PlannedWebLoad.java
+++ app/src/main/java/rooland/cz/webpagerotator/PlannedWebLoad.java
@@ -1,56 +1,9 @@
 package rooland.cz.webpagerotator;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import java.util.TimerTask;
 
-public class  PlannedWebLoad extends TimerTask {
-    public AppCompatActivity localActivity;
-
-    public Integer pages_position;
-    public ProgressBar timer_progress_bar;
-    public Integer fraction;
-    public String[] pages;
-
-
-    public PlannedWebLoad(AppCompatActivity activity, ProgressBar inputProgressBar, Integer inputFraction) {
-        pages = new String[] {
-                "http://www.rooland.cz/tag/dev.html",
-                "http://www.rooland.cz/tag/komunikace.html",
-                "http://www.rooland.cz/tag/zacatecnik.html"
-        };
-
-        pages_position = 0;
-        localActivity = activity;
-        timer_progress_bar = inputProgressBar;
-        fraction = inputFraction;
-    }
-
-
-    public void run() {
-        //This method will be called from another thread,and Contacts.Intents.UI work must
-        // happen in the main thread,so we dispatch it via a Handler object.
-
-        timer_progress_bar.incrementProgressBy(fraction);
-
-        if (timer_progress_bar.getProgress() == timer_progress_bar.getMax()) {
-            timer_progress_bar.setProgress(0);
-            localActivity.webview.loadUrl(next_page());
-        }
-    }
-
-    public String next_page() {
-
-        next_page_address = pages[pages_position];
-
-        if(pages_position > pages.size()) {
-            pages_position += 1;
-        } else {
-            pages_position = 0;
-        }
-
-        return next_page_address;
-    }
-}
